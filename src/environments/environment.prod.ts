5b152e7b476422403099f247aec91eadcaf83071
Kamran <oppoand54321@gmail.com>
2026-09-12 06:15:37 +0500

    update environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://93.127.135.2/api/v1',
-  wsUrl: 'ws://93.127.135.2'
+  apiUrl: 'http://93.127.135.2:8000/api/v1',
+  wsUrl: 'ws://93.127.135.2:8000'
 };
