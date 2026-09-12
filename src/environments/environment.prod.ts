127d4f519d4a415df3b0ba1a48d4203f3d087541
Kamran <oppoand54321@gmail.com>
2026-09-12 17:23:03 +0500

    Add vercel.json and update environment configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://93.127.135.2:8000/api/v1',
-  wsUrl: 'ws://93.127.135.2:8000'
+  apiUrl: 'https://lang.knova.work/api/v1',
+  wsUrl: 'wss://lang.knova.work'
 };
